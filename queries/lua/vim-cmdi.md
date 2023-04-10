27cf4b0b3fa289c565540e55014682aa000b10d0
GeekMasher <geekmasher@gmail.com>
2023-04-10 01:58:52 +0100

    feat(queries): Lua Vim query to find CMDi

---
+++ queries/lua/vim-cmdi.md
@@ -0,0 +1,5 @@
+---
+name: Nvim CMD Injection 
+severity: warning 
+
+---
---
+++ queries/lua/vim-cmdi.scm
@@ -0,0 +1,8 @@
+;;
+(function_call
+  name: (dot_index_expression) @name (#eq? @name "vim.fn.jobstart")
+  (arguments 
+    (identifier) @result
+  )
+)
+
