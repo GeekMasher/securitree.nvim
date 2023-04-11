91519d605b26c25c0b4d32112d548135f2ddfc25
GeekMasher <geekmasher@gmail.com>
2023-04-11 19:50:05 +0100

    feat(queries): Update Rust Locals for better use support

--- queries/rust/locals.scm
+++ queries/rust/locals.scm
@@ -1,23 +1,35 @@
 ;; https://github.com/nvim-treesitter/nvim-treesitter/blob/master/queries/rust/locals.scm
-(use_declaration 
-  (scoped_identifier
-    (scoped_identifier) @module
-    (identifier) @import
-  )
-)
 
 (use_declaration 
   (scoped_use_list 
-    (scoped_identifier) @module
+    [
+      (scoped_identifier) @module
+      (identifier) @module
+    ]
     (use_list
       (identifier) @import
     )
   )
 )
 
+(use_declaration 
+  (scoped_use_list 
+    (identifier) @module
+    (use_list
+      (scoped_identifier
+        path: (identifier) @module
+        name: (identifier) @import
+      )
+    )
+  )
+) 
+
 (use_declaration
   (scoped_identifier
-    path: (identifier) @module
+    [
+      path: (scoped_identifier) @module
+      path: (identifier) @module
+    ]
     name: (identifier) @import
   )
 )
